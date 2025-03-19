ef88542ef2190d37df37509c5f0172c4f1944eef
sydney <kimsydney18@gmail.com>
2025-03-19 17:06:13 -0400

    not working?

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
@@ -67,6 +67,7 @@ public class CourseServiceImpl implements CourseServiceInterface {
             course.setCourseName("IDs do not match, course not updated");
             course.setCourseDesc("IDs do not match, course not updated");
         }
+
         courseDao.updateCourse(course);
 
         return course;
